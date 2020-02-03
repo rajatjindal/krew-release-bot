612eb6d5d9d16eac2c68764eda19c2a34ada6f4f
Rajat Jindal <rajatjindal83@gmail.com>
2020-02-03 21:14:24 +0530

    use token

--- pkg/source/actions/action_runner.go
+++ pkg/source/actions/action_runner.go
@@ -19,6 +19,7 @@ import (
 
 func getHTTPClient() *http.Client {
 	if os.Getenv("GITHUB_TOKEN") != "" {
+		logrus.Info("GITHUB_TOKEN env variable found, using authenticated requests.")
 		ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")})
 		return oauth2.NewClient(context.TODO(), ts)
 	}
