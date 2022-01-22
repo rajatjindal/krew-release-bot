621a27fe9ae7df60081852d2106f30a1e82a57bc
Rajat Jindal <rajatjindal83@gmail.com>
2022-01-22 17:33:09 +0530

    update

---
+++ req.sh
@@ -0,0 +1,10 @@
+curl -vXPOST 'http://localhost:8080/' -d'{
+	"tagName": "v0.0.38",
+	"pluginName": "whoami",
+	"pluginRepo": "kubectl-whoami",
+	"pluginOwner": "rajatjindal",
+	"pluginReleaseActor": "rajatjindal",
+	"templateFile": "",
+	"processedTemplate": ""
+
+}'
