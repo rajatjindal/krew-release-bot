36a3caadb4225a1dbffd8ab0e9ce5fa5e0fb4044
Rajat Jindal <rajatjindal83@gmail.com>
2026-05-16 12:28:03 +0530

    extract pull request opening from releaser

---
+++ pkg/releaser/utils_test.go
@@ -0,0 +1,64 @@
+package releaser
+
+import (
+	"testing"
+
+	"github.com/rajatjindal/krew-release-bot/pkg/source"
+)
+
+type fakePullRequestOpener struct {
+	owner       string
+	repo        string
+	pullRequest PullRequest
+	response    string
+}
+
+func (f *fakePullRequestOpener) Open(owner, repo string, pullRequest PullRequest) (string, error) {
+	f.owner = owner
+	f.repo = repo
+	f.pullRequest = pullRequest
+	return f.response, nil
+}
+
+func TestOpenPullRequestUsesConfiguredOpener(t *testing.T) {
+	opener := &fakePullRequestOpener{response: "https://example.com/pr/1"}
+	r := &Releaser{
+		TokenUserHandle:            "bot-user",
+		PullRequestOpener:          opener,
+		UpstreamKrewIndexRepoOwner: "kubernetes-sigs",
+		UpstreamKrewIndexRepo:      "krew-index",
+	}
+
+	request := &source.ReleaseRequest{
+		TagName:            "v1.2.3",
+		PluginName:         "my-plugin",
+		PluginRepo:         "my-plugin",
+		PluginOwner:        "acme",
+		PluginReleaseActor: "release-user",
+	}
+
+	prURL, err := r.openPullRequest(request)
+	if err != nil {
+		t.Fatalf("openPullRequest returned error: %v", err)
+	}
+
+	if prURL != "https://example.com/pr/1" {
+		t.Fatalf("unexpected pr url: %s", prURL)
+	}
+
+	if opener.owner != "kubernetes-sigs" || opener.repo != "krew-index" {
+		t.Fatalf("unexpected target repo: %s/%s", opener.owner, opener.repo)
+	}
+
+	if opener.pullRequest.Base != "master" {
+		t.Fatalf("unexpected base branch: %s", opener.pullRequest.Base)
+	}
+
+	if opener.pullRequest.Head != "bot-user:acme-my-plugin-my-plugin-v1.2.3" {
+		t.Fatalf("unexpected head: %s", opener.pullRequest.Head)
+	}
+
+	if opener.pullRequest.Title != "release new version v1.2.3 of my-plugin" {
+		t.Fatalf("unexpected title: %s", opener.pullRequest.Title)
+	}
+}
